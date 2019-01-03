892d216306b35ed4da19a323904c37d430bb1dad
Will Charczuk <will@blend.com>
2019-01-02 17:15:26 -0800

    'fixing tests'

--- pkg/engine/template_test.go
+++ pkg/engine/template_test.go
@@ -4,21 +4,11 @@ import (
 	"testing"
 
 	"github.com/blend/go-sdk/assert"
-	"github.com/blend/go-sdk/exception"
 	"github.com/wcharczuk/blogctl/pkg/model"
 )
 
 func TestPartitionPosts(t *testing.T) {
 	assert := assert.New(t)
-
-	_, err := partition(0, 0, nil)
-	assert.True(exception.Is(err, ErrPartitionCountInvalid))
-
-	_, err = partition(1, 1, nil)
-	assert.True(exception.Is(err, ErrPartitionIndexTooLarge))
-
-	_, err = partition(2, 1, nil)
-	assert.True(exception.Is(err, ErrPartitionIndexTooLarge))
 
 	posts := []model.Post{
 		{Meta: model.Meta{Title: "one"}},
@@ -29,9 +19,6 @@ func TestPartitionPosts(t *testing.T) {
 		{Meta: model.Meta{Title: "six"}},
 		{Meta: model.Meta{Title: "seven"}},
 	}
-
-	_, err = partition(0, 8, posts)
-	assert.True(exception.Is(err, ErrPartitionCountTooLarge))
 
 	partition0, err := partition(0, 2, posts)
 	assert.Nil(err)
